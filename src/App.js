deaa3df23586638d6ae34bfd73aa583a8805f348
Devdeep0109 <devdeepgoyal@gmail.com>
2023-10-11 01:05:54 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {data} from "./data.js";
+import {useState} from "react";
 
 function App() {
+  console.log(data);
+  const [search ,setSearch] = useState("");
+
+  function handleChange(e){
+    setSearch(e.target.value);
+  }
+  console.log(search);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="inner">
+        <h1>Search Filter</h1>
+        <form action="">
+          <input type="textArea"  onChange={handleChange} />
+        </form>
+        <table>
+          <tr>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+          {data.filter( (item) =>{
+            return search.toLowerCase() === '' ?
+            item : item.first_name.toLowerCase().includes(search.toLowerCase());
+          }).map( (item) =>(
+            <tr key ={item.id}>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+            <td>{item.email}</td>
+            <td>{item.phone}</td>
+            
+          </tr>
+          ) )}
+          
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,302 @@
+export const data =[{
+    "id": 1,
+    "first_name": "Hobie",
+    "last_name": "Hevey",
+    "email": "hhevey0@mozilla.org",
+    "phone": "651 870 0455"
+  }, {
+    "id": 2,
+    "first_name": "Arlen",
+    "last_name": "McQuarrie",
+    "email": "amcquarrie1@shop-pro.jp",
+    "phone": "487 858 9846"
+  }, {
+    "id": 3,
+    "first_name": "Cly",
+    "last_name": "Athelstan",
+    "email": "cathelstan2@globo.com",
+    "phone": "849 556 2487"
+  }, {
+    "id": 4,
+    "first_name": "Alameda",
+    "last_name": "Teck",
+    "email": "ateck3@blogtalkradio.com",
+    "phone": "373 142 7544"
+  }, {
+    "id": 5,
+    "first_name": "Lauraine",
+    "last_name": "Suett",
+    "email": "lsuett4@addthis.com",
+    "phone": "271 257 1929"
+  }, {
+    "id": 6,
+    "first_name": "Abramo",
+    "last_name": "Chippin",
+    "email": "achippin5@skype.com",
+    "phone": "882 910 7326"
+  }, {
+    "id": 7,
+    "first_name": "Kalil",
+    "last_name": "Edelman",
+    "email": "kedelman6@senate.gov",
+    "phone": "590 231 3525"
+  }, {
+    "id": 8,
+    "first_name": "Mar",
+    "last_name": "Thews",
+    "email": "mthews7@wiley.com",
+    "phone": "205 419 2370"
+  }, {
+    "id": 9,
+    "first_name": "Nigel",
+    "last_name": "Vasilov",
+    "email": "nvasilov8@home.pl",
+    "phone": "588 421 9563"
+  }, {
+    "id": 10,
+    "first_name": "Mercedes",
+    "last_name": "Lambertson",
+    "email": "mlambertson9@census.gov",
+    "phone": "936 568 9098"
+  }, {
+    "id": 11,
+    "first_name": "Bancroft",
+    "last_name": "De Bell",
+    "email": "bdebella@huffingtonpost.com",
+    "phone": "298 530 9476"
+  }, {
+    "id": 12,
+    "first_name": "Broderick",
+    "last_name": "Rosendorf",
+    "email": "brosendorfb@skyrock.com",
+    "phone": "552 876 2941"
+  }, {
+    "id": 13,
+    "first_name": "Dianna",
+    "last_name": "Maulin",
+    "email": "dmaulinc@artisteer.com",
+    "phone": "121 789 7463"
+  }, {
+    "id": 14,
+    "first_name": "Rickey",
+    "last_name": "Shasnan",
+    "email": "rshasnand@auda.org.au",
+    "phone": "595 326 5947"
+  }, {
+    "id": 15,
+    "first_name": "Trina",
+    "last_name": "Heys",
+    "email": "theyse@cbsnews.com",
+    "phone": "956 924 4032"
+  }, {
+    "id": 16,
+    "first_name": "Marcel",
+    "last_name": "Schubbert",
+    "email": "mschubbertf@marriott.com",
+    "phone": "886 401 9174"
+  }, {
+    "id": 17,
+    "first_name": "Jewel",
+    "last_name": "Hallibone",
+    "email": "jhalliboneg@guardian.co.uk",
+    "phone": "831 985 8175"
+  }, {
+    "id": 18,
+    "first_name": "Morgen",
+    "last_name": "Forsyde",
+    "email": "mforsydeh@google.fr",
+    "phone": "149 335 2327"
+  }, {
+    "id": 19,
+    "first_name": "Wain",
+    "last_name": "Dobrovsky",
+    "email": "wdobrovskyi@ihg.com",
+    "phone": "976 264 2295"
+  }, {
+    "id": 20,
+    "first_name": "Kendra",
+    "last_name": "Whetland",
+    "email": "kwhetlandj@etsy.com",
+    "phone": "901 893 4986"
+  }, {
+    "id": 21,
+    "first_name": "Valencia",
+    "last_name": "Rummin",
+    "email": "vrummink@edublogs.org",
+    "phone": "793 893 2716"
+  }, {
+    "id": 22,
+    "first_name": "Peter",
+    "last_name": "Hamshere",
+    "email": "phamsherel@jigsy.com",
+    "phone": "697 602 2022"
+  }, {
+    "id": 23,
+    "first_name": "Wendeline",
+    "last_name": "MacVicar",
+    "email": "wmacvicarm@usnews.com",
+    "phone": "588 592 1164"
+  }, {
+    "id": 24,
+    "first_name": "Foster",
+    "last_name": "Ferrers",
+    "email": "fferrersn@google.ca",
+    "phone": "426 372 9529"
+  }, {
+    "id": 25,
+    "first_name": "Dorie",
+    "last_name": "Faivre",
+    "email": "dfaivreo@google.ca",
+    "phone": "311 763 9837"
+  }, {
+    "id": 26,
+    "first_name": "Marcellina",
+    "last_name": "Dutch",
+    "email": "mdutchp@blogtalkradio.com",
+    "phone": "226 346 5641"
+  }, {
+    "id": 27,
+    "first_name": "Bron",
+    "last_name": "Wissby",
+    "email": "bwissbyq@hhs.gov",
+    "phone": "269 284 2004"
+  }, {
+    "id": 28,
+    "first_name": "Dominick",
+    "last_name": "Joerning",
+    "email": "djoerningr@naver.com",
+    "phone": "575 784 3339"
+  }, {
+    "id": 29,
+    "first_name": "Mimi",
+    "last_name": "Kleinschmidt",
+    "email": "mkleinschmidts@zimbio.com",
+    "phone": "984 278 1108"
+  }, {
+    "id": 30,
+    "first_name": "Gusta",
+    "last_name": "Chaloner",
+    "email": "gchalonert@so-net.ne.jp",
+    "phone": "797 789 8762"
+  }, {
+    "id": 31,
+    "first_name": "Mame",
+    "last_name": "Choppen",
+    "email": "mchoppenu@hhs.gov",
+    "phone": "864 440 7414"
+  }, {
+    "id": 32,
+    "first_name": "Moira",
+    "last_name": "Nannini",
+    "email": "mnanniniv@webnode.com",
+    "phone": "793 602 1518"
+  }, {
+    "id": 33,
+    "first_name": "Myrna",
+    "last_name": "Adams",
+    "email": "madamsw@sciencedaily.com",
+    "phone": "112 407 8650"
+  }, {
+    "id": 34,
+    "first_name": "Eachelle",
+    "last_name": "Tows",
+    "email": "etowsx@comsenz.com",
+    "phone": "581 130 0456"
+  }, {
+    "id": 35,
+    "first_name": "Anselm",
+    "last_name": "Mayall",
+    "email": "amayally@eventbrite.com",
+    "phone": "356 669 6974"
+  }, {
+    "id": 36,
+    "first_name": "Cindi",
+    "last_name": "Negri",
+    "email": "cnegriz@stumbleupon.com",
+    "phone": "361 213 0846"
+  }, {
+    "id": 37,
+    "first_name": "Angie",
+    "last_name": "Alvarado",
+    "email": "aalvarado10@odnoklassniki.ru",
+    "phone": "991 691 9294"
+  }, {
+    "id": 38,
+    "first_name": "Marys",
+    "last_name": "Hirtz",
+    "email": "mhirtz11@lulu.com",
+    "phone": "823 383 4687"
+  }, {
+    "id": 39,
+    "first_name": "Willamina",
+    "last_name": "Batterham",
+    "email": "wbatterham12@goodreads.com",
+    "phone": "461 473 8582"
+  }, {
+    "id": 40,
+    "first_name": "Tamarah",
+    "last_name": "Dufton",
+    "email": "tdufton13@dyndns.org",
+    "phone": "798 923 5798"
+  }, {
+    "id": 41,
+    "first_name": "Zachary",
+    "last_name": "Burnage",
+    "email": "zburnage14@narod.ru",
+    "phone": "390 682 5936"
+  }, {
+    "id": 42,
+    "first_name": "Dex",
+    "last_name": "Leaman",
+    "email": "dleaman15@nyu.edu",
+    "phone": "128 723 5707"
+  }, {
+    "id": 43,
+    "first_name": "Hamid",
+    "last_name": "Iddison",
+    "email": "hiddison16@newyorker.com",
+    "phone": "772 755 7155"
+  }, {
+    "id": 44,
+    "first_name": "Nike",
+    "last_name": "Pie",
+    "email": "npie17@smugmug.com",
+    "phone": "290 935 7008"
+  }, {
+    "id": 45,
+    "first_name": "Melli",
+    "last_name": "Von Der Empten",
+    "email": "mvonderempten18@usa.gov",
+    "phone": "300 344 0588"
+  }, {
+    "id": 46,
+    "first_name": "Joshua",
+    "last_name": "Colebeck",
+    "email": "jcolebeck19@tamu.edu",
+    "phone": "880 497 1513"
+  }, {
+    "id": 47,
+    "first_name": "Nady",
+    "last_name": "Iwanowicz",
+    "email": "niwanowicz1a@gmpg.org",
+    "phone": "406 617 7241"
+  }, {
+    "id": 48,
+    "first_name": "Vonny",
+    "last_name": "Glaserman",
+    "email": "vglaserman1b@i2i.jp",
+    "phone": "666 672 2087"
+  }, {
+    "id": 49,
+    "first_name": "Lebbie",
+    "last_name": "Langston",
+    "email": "llangston1c@eepurl.com",
+    "phone": "966 303 5224"
+  }, {
+    "id": 50,
+    "first_name": "Roselin",
+    "last_name": "Branney",
+    "email": "rbranney1d@paginegialle.it",
+    "phone": "975 441 3320"
+  }]
+  
